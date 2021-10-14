062a6409cc970464110dc06f09de3fbf669f0d46
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-10-14 14:08:33 +0100

    Create Main.java

---
+++ src/main/java/gsonsample/Main.java
@@ -0,0 +1,36 @@
+package gsonsample;
+
+import java.util.Scanner;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+import gsonsample.persona;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		persona p=new persona();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("(Obligatorio) Introduce el nombre:");
+		p.nombre = scanner.nextLine();
+		 while (!p.nombre.matches("^[a-zA-Z]+$")) {
+	            System.out.println("Introduzca otro nombre:");
+	            p.nombre = scanner.nextLine();
+	        }
+		 System.out.println("(Obligatorio) Introduce los apellidos:");
+		p.apellido = scanner.nextLine();
+		while (!p.apellido.matches("^[a-zA-Z]+( [a-zA-Z]+)*$")) {
+            System.out.println("Introduzca otros apellidos");
+             p.apellido = scanner.nextLine();
+        }
+		System.out.println("(Obligatorio) Introduce la edad:");
+		p.edad = scanner.nextInt();
+		com.google.gson.Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(p);
+		System.out.println(json);
+		scanner.close();
+
+	}
+
+}
